00fbe25b592a304cc1add5dc2ef45ade23819814
lahad230 <lahad230@gmail.com>
2021-04-12 12:10:49 +0300

    init commit

---
+++ db.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# <description>
+# This script will build and deploy postgres DB automatically.
+# This script works on Ubuntu operation system.
+var=$(psql -v 2>&1)
+pass=$1
+
+function  usage() {
+	#<description>
+	# This function print the help menu for the user.
+    echo "call this script with 1 argument of the postgres user password:
+                
+	Example: ./db.sh 'password'" ; 
+    exit 1; 
+}
+
+function  validation(){
+	#<description>
+	# This function valdate all the arguments from the user.
+
+    if [ -z "${pass}" ]; then
+        usage
+    fi
+}
+
+function installtion(){
+	#<description>
+	# This function install using apt all the  requirements for the db.
+
+    sudo apt-get update
+    #sudo apt-get install git
+    #git clone https://github.com/lahad230/weightTracker.git
+    sudo apt-get -y install postgresql    
+}
+
+function editConfig(){
+    #<description>
+    #this function makes changes to pg_hba.conf and postgresql.conf
+    #to make the db listen to the outside.
+    echo "host    all             all             all                     md5" | sudo tee -a /etc/postgresql/$var/main/pg_hba.conf
+    echo "listen_addresses = '*'" | sudo tee -a /etc/postgresql/$var/main/postgresql.conf
+}
+
+function initDb(){
+    #<description>
+    #this function creates the table in the db and adds password to the postgres user.
+    sudo -u postgres psql -d postgres -c "CREATE TABLE IF NOT EXISTS measurements (id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY, user_id varchar(50) NOT NULL, measure_date date NOT NULL, weight numeric(5,1) NOT NULL)"
+    sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD '${pass}';"
+}
+
+function enableService(){
+    #<description>
+    #this function enables the postgres service.
+    sudo systemctl enable postgresql
+}
+
+validation
+installtion
+editConfig
+initDb
+enableService
